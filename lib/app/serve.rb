78eff987c9be94ec46faea29fc1269f57d303731
ngrichyj4 <ngrichyj4@gmail.com>
2013-11-20 10:27:39 -0700

    Fix indent

--- lib/app/serve.rb
+++ lib/app/serve.rb
@@ -9,7 +9,7 @@ class ExactAPI < Grape::API
       
       # Logs to console output
       def logger
-    	request.logger
+        request.logger
       end
 
       # Check if user is in store
@@ -23,52 +23,50 @@ class ExactAPI < Grape::API
       end
 
       def exists?(params)
-		error!('Password empty', 500) if params["password"].empty?
-		true
+        error!('Password empty', 500) if params["password"].empty?
+        true
       end
 
     end
 
     # User routes
     resource :users do
-	  
-	  desc "Create user."
-	  post :create do
-		
-	  	params = JSON.parse(request.body.read)
-	  	logger.info params
-	
-	    # Check for duplicates and check password
-	    if  exists?(params)
-	    	
-	    	        begin
-				User.create(params)
-				status 200
-			rescue => error
-				error!(error, 500)
-			end
-				
-	     end
-	
-	  end
-	
-	  
-	  desc "Authenticate user."
-	  post :authenticate do 
-	 
-	  	# Perform authentication step
-	  	authenticate!
-	  	status 200
-	  end
-	
+      
+      desc "Create user."
+      post :create do
+        
+        params = JSON.parse(request.body.read)
+        logger.info params
+
+        # Check for duplicates and check password
+        if  exists?(params)
+            
+            begin
+                User.create(params)
+                status 200
+            rescue => error
+                error!(error, 500)
+            end
+                
+        end
+
+      end
+
+      
+      desc "Authenticate user."
+      post :authenticate do 
+     
+        # Perform authentication step
+        authenticate!
+        status 200
+      end
+
     end
 
 end
 
 class Web < Sinatra::Base
-  
-   get '/' do
-	"Working."
-   end
-  
+  get '/' do
+    "Working."
+  end
 end
